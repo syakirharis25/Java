2748247db3174368e294e421d478208cdf52a8dc
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-04-20 22:00:58 +0800

    new file:   Tutorial Java/08 - Variable/.idea/misc.xml         new file:   Tutorial Java/08 - Variable/.idea/modules.xml         new file:   Tutorial Java/08 - Variable/.idea/vcs.xml         new file:   Tutorial Java/08 - Variable/.idea/workspace.xml         new file:   Tutorial Java/08 - Variable/08 - Variable.iml         new file:   Tutorial Java/08 - Variable/out/production/08 - Variable/com/tutorial/Main.class         new file:   Tutorial Java/08 - Variable/src/com/tutorial/Main.java

---
+++ Tutorial Java/08 - Variable/src/com/tutorial/Main.java
@@ -0,0 +1,22 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+
+        // kita akan membuat variabel
+        // tipe data
+        int a = 10; // assignment
+        System.out.println("nilai x = " + a);
+
+        a = 20;
+        System.out.println("nilai x baru = " + a);
+
+        // kita akan membuat sebuah deklarasi
+        int b;
+
+        b = 7;
+
+        System.out.println("nilai b = " + b);
+
+    }
+}
